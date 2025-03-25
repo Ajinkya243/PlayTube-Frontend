8bd112291bb05ed9429f361666982c7e016fb644
Ajinkya243 <ajinkyagund2@gmail.com>
2025-03-25 16:47:37 +0530

    Add navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import Sidebar from './components/sidebar/Sidebar';
 import { fetchVideos } from './utils/redux/slice/videoSlice';
 import { useDispatch } from 'react-redux';
 import Videos from './pages/videos page/Videos';
+import { useGlobalState } from './utils/context/GlobalStateProvider';
+import { fetchVideoData } from './utils/redux/slice/videoDataSlice';
 
 function App() {
   const dispatch=useDispatch();
+  const{setCurrentPage}=useGlobalState();
   useEffect(()=>{
-    dispatch(fetchVideos());
-  },[])
+    dispatch(fetchVideoData());
+    dispatch(fetchVideoData())
+    setCurrentPage('All');
+  },[dispatch])
   return(
   <>
   <Navbar/>
